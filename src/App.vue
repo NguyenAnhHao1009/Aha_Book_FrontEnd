8b1df6af1d4f3979135f54fea957cb4f8dc71227
NguyenAnhHao1009 <anhhaoaoao@gmail.com>
2024-10-22 00:13:15 +0700

    Cau hinh cho du an vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    Header
+  </header>
+
+  <main>
+  </main>
+  <footer>
+    Footer
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
